f34122111d2061cce29598a6545acb3798769669
Lenovo <Lenovo@DESKTOP-067GH63>
2022-05-11 09:59:44 +0430

    add project file and first push

---
+++ CardType.cs
@@ -0,0 +1,12 @@
+﻿namespace FactoryMethod
+{
+    public abstract class CardType
+    {
+        protected double CreditLimit;
+        public abstract void setCreditLimit();
+        public override string ToString()
+        {
+            return "Your card is " + this.GetType().Name + " & your credit limit is " + CreditLimit;
+        }
+    }
+}
